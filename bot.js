c4711730baaa4f00e9297e52211510e2561f4c41
Reem35 <43762012+Reem35@users.noreply.github.com>
2018-10-23 19:54:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,5 +29,45 @@ message.guild.members.forEach(m => {
   }
  
 });
-
+lient.on("message", msg => {//By DEL.25♥♥#1406 || هاشم || Alpha Codes || All copyrights for me & Alpha Codes.
+var prefix = '-';// البرفكس
+var m = msg.guild.name
+var d = msg.guild.memberCount
+var p = msg.guild.roles.size
+var c = msg.guild.channels.size
+var l = msg.guild.region
+var o = msg.guild.iconURL
+var k = msg.guild.owner
+var i = msg.guild.emojis.size
+var b = msg.guild.members.filter(m => m.user.bot).size
+var h = d - b
+var cre = `${moment(msg.guild.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(msg.guild.createdAt).fromNow()}\``
+var t = msg.guild.channels.filter(e => e.type === "text")
+var v = msg.guild.channels.filter(e => e.type === "voice")
+var pow = msg.guild.verificationLevel
+var e = msg.guild.emojis.size
+var e2 = msg.guild.emojis.array()
+var afk = msg.guild.afkChannel
+var ID = msg.guild.id
+if (msg.content.startsWith(prefix + "معلومات السيرفر")){
+var embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setThumbnail(o)
+.addField("👑**ريْس السيرفر**👑⤵", k, true)
+.addField("📜**اسم السيرفر**📜⤵", m, true)
+.addField("🆔**ايدي السيرفر**🆔⤵", ID, true)
+.addField("👥**كل الاعضاء**🤖⤵", d, true)
+.addField("📕**الرتب**📕⤵", p, true)
+.addField("📕**القنوات**📕⤵", c, true)
+.addField("🌐**نوع السيرفر**🌐⤵", l, true)
+.addField("🤖**البوتات**🤖⤵", b, true)
+.addField("👥**الاشخاص**👥⤵", h, true)
+.addField("📝**الرومات الكتابيه**📝⤵", `${t.size}`, true)
+.addField("🔒**مستوى حماية السيرفر**🔒⤵", pow, true)
+.addField("🎤**الرومات الصوية**🎤⤵", `${v.size}`, true)
+.addField("📆**تم انشاء السيرفر في**📆⤵", cre,true)
+.addField("🛏غرفت النوم🛏⤵", afk, true)
+msg.channel.sendEmbed(embed);
+}
+});//حقوق الفا كودز || هاشم ♥ || Alpha Codes || All copyrights for me & Alpha Codes.
 client.login(process.env.BOT_TOKEN);
